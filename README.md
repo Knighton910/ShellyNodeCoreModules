6afa4522996a85df09cfc89f60f5a96a76b81ad4
lordKnighton <keltheceo@gmail.com>
2019-01-08 12:50:35 -0800

    addages on Readme + Connect Souce code

--- README.md
+++ README.md
@@ -2,4 +2,12 @@ LEARNING NODE author POWERS publisher O'REILLY
 
 Deep diving into Node Core Modules &well Connect and Express
 
-An Anceint book in Modern Node times, but has great context to consume.
+An Ancient book in Modern Node times, but has great context to consume.
+
+
+
+
+|NOTE| because im using an old version of node with this book
+i had to install an old version of MIME package
+i had trouble installing it, so i had to run this command, be sure to reset to (true)
+after this project : npm config set strict-ssl false
--- index.js
+++ index.js
@@ -1,3 +1,47 @@
+var http = require('http'),
+    url =  require('url'),
+    fs =   require('fs'),
+    mime = require('mime'),
+    base = '/home/examples/public_html';
+
+http.createServer(function (req, res) {
+
+    pathname = base + req.url;
+    console.log(pathname);
+
+    fs.stat(pathname, function(err, stats) {
+        if (err) {
+            res.writeHead(404);
+            res.write('Bad request 404\n');
+            res.end();
+        } else if (stats.isFile()) {
+            // content type
+            var type = mime.lookup(pathname);
+            console.log(type);
+            res.setHeader('Content-Type', type);
+
+            // 200 status - found, no errors
+            res.statusCode = 200;
+
+            // create and pipe readable stream
+            var file = fs.createReadStream(pathname);
+            file.on('open', function() {
+
+                file.pipe(res);
+            });
+            file.on('error', function(err) {
+                console.log(err);
+            });
+        } else {
+            res.writeHead(403);
+            res.write('Directory access is forbidden');
+            res.end();
+        }
+    });
+}).listen(8124);
+console.log('Server running @ 8124/');
+
+
 // const http = require('http');
 //
 // // The url we want, plus the path and options we need
@@ -31,13 +75,13 @@
 //     return globalValue;
 // };
 
-var eventEmitter = require('events').EventEmitter;
-var counter = 0;
-
-var em = new eventEmitter()
-
-setInterval(function(){em.emit(33, counter++);},3000);
-
-em.on(33, function(data) {
-    console.log('Num 33 ' + data);
-});
+// var eventEmitter = require('events').EventEmitter;
+// var counter = 0;
+//
+// var em = new eventEmitter()
+//
+// setInterval(function(){em.emit(33, counter++);},3000);
+//
+// em.on(33, function(data) {
+//     console.log('Num 33 ' + data);
+// });
